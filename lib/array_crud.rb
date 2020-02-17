1fa541a21607e9091de46bfadb7837862bf224ad
Samantha Barros <samantha.barros95@gmail.com>
2020-02-17 03:09:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  puts create_an_array.inspect #create_an_array.pop
+  create_an_array.push.pop
 end
 
 def remove_element_from_start_of_array(array)
